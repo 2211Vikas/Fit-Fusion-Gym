289345138eaf49eb9e2c9e0bc0d08a09cd9899f8
2211Vikas <vc221101@gmail.com>
2023-07-30 01:27:33 +0530

    Fit Fusion Gym Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => (
       <img src={Logo} alt="logo" style={{ width: '250px', height: '100px' }} />
     </Stack>
     <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">
-      A react project by <br/>
+      A react project by 
       <Link to="https://2211vikas.github.io/Portfolio-Page/">
         Vikas Chaudhary
       </Link> 
