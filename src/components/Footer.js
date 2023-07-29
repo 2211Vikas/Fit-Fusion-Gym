c87cd9a5b050a9c8b701c5b56b263db6fabe61c9
2211Vikas <vc221101@gmail.com>
2023-07-30 01:18:01 +0530

    Fit Fusion Gym Page

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
+import { Link } from 'react-router-dom'
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '250px', height: '100px' }} />
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">
+      A react project by <br/>
+      <Link to="https://2211vikas.github.io/Portfolio-Page/">
+        Vikas Chaudhary
+      </Link> 
+    </Typography>
+    
+  </Box>
+);
+
+export default Footer;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '2fcd4fa606msh99a038b7c3dd4f6p1da185jsn8d0ea6ec444c',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '2fcd4fa606msh99a038b7c3dd4f6p1da185jsn8d0ea6ec444c',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
